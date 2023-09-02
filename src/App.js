18ee77264ef7855dbc41b1942802e089486ae61c
Nick <nikitalytvynov0506@gmail.com>
2023-09-02 15:49:28 +0300

    siii

--- src/App.js
+++ src/App.js
@@ -1,18 +1,71 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+
+//"server" : "json-server --port 3001 --watch db.json --host 127.0.0.1"
 
 function App() {
-    const [books, setBooks] = useState([])
+    const [books, setBooks] = useState([]);
 
-    const createBook = (title) => {
-        console.log('Need t0 add book with:', title)
+    
+
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3002/books');
+
+        setBooks(response.data);
+    };
+
+    useEffect(() => {
+        fetchBooks();
+    }, []);
+
+    const editBookById = (id, newTitle) => {
+        const updatedBooks = books.map((book) => {
+            if (book.id === id) {
+                return { ...book, title: newTitle }
+            }
+
+            return book;
+        });
+
+        setBooks(updatedBooks)
+    }
+
+    const deleteBookById = () => {
+        const updatedBooks = books.filter((book, id) => {
+            return book.id !== id;
+        })
+
+        setBooks(updatedBooks)
+    };
+
+    const createBook = async (title) => {
+        const response = await axios.post('http://localhost:3002/books', {
+            title
+        });
+
+        const updatedBooks = [
+            ...books,
+            response.data
+        ]
+        setBooks(updatedBooks)
+        console.log(response)
+        // const updatedBooks = [
+        //     ...books,
+        //     { id: Math.round(Math.random() * 9999), title: title }
+        // ];
+        // setBooks(updatedBooks);
     };
 
     return (
-    <div>
+    <div className="app">
+        <h1>Reading List</h1>
+        <BookList onEdit = {editBookById} books={books} onDelete={deleteBookById}/>
         <BookCreate onCreate={createBook} />
     </div>
     );
 }
+
+
 
 export default App;
