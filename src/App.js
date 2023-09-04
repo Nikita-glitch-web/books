4d4ebb3c005f4eedae77a9e80a58f3b7d9f0707d
Nick <nikitalytvynov0506@gmail.com>
2023-09-04 21:17:57 +0300

    si

--- src/App.js
+++ src/App.js
@@ -6,12 +6,8 @@ import BookList from "./components/BookList";
 
 function App() {
     const [books, setBooks] = useState([]);
-
-    
-
     const fetchBooks = async () => {
         const response = await axios.get('http://localhost:3002/books');
-
         setBooks(response.data);
     };
 
@@ -19,19 +15,23 @@ function App() {
         fetchBooks();
     }, []);
 
-    const editBookById = (id, newTitle) => {
+    const editBookById = async (id, newTitle) => {
+        const response =  await axios.put(`http://localhost:3002/books/${id}`, {
+            title: newTitle
+        });
         const updatedBooks = books.map((book) => {
             if (book.id === id) {
-                return { ...book, title: newTitle }
+                return { ...book, ...response.data}
             }
 
             return book;
         });
 
         setBooks(updatedBooks)
-    }
+    };
 
-    const deleteBookById = () => {
+    const deleteBookById = async (id) => {
+        await axios.delete(`http://localhost:3001/books/${id}`)
         const updatedBooks = books.filter((book, id) => {
             return book.id !== id;
         })
